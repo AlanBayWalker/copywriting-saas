23b4516ebe18190646b9376264f0759bbafbe41a
Alan <kanonbolt128@gmail.com>
2019-08-14 22:11:07 -0500

    Updates

---
+++ precache-manifest.d5cb5cba59eaa93ccfc38b31695d4a67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2346e06a8d714c4d563a64a617b2dba6",
+    "url": "/copywriting-saas/index.html"
+  },
+  {
+    "revision": "7e0e75d135e1fda73335",
+    "url": "/copywriting-saas/static/js/2.739a9a12.chunk.js"
+  },
+  {
+    "revision": "20948735393fee28f42c",
+    "url": "/copywriting-saas/static/js/main.c9195bc0.chunk.js"
+  },
+  {
+    "revision": "acd4dc873a9898668982",
+    "url": "/copywriting-saas/static/js/runtime~main.a1245284.js"
+  },
+  {
+    "revision": "af82c420d06ed0d4aa803fa586d6bec1",
+    "url": "/copywriting-saas/static/media/item-thumbnail.af82c420.png"
+  },
+  {
+    "revision": "370a9c964075cb2b9040ab2599a78a49",
+    "url": "/copywriting-saas/static/media/profile-avatar.370a9c96.jpg"
+  },
+  {
+    "revision": "a73f88252b0478fcd4b8c8d68d511584",
+    "url": "/copywriting-saas/static/media/profile-background.a73f8825.png"
+  }
+]);
